10e2cd9e4bfc2cede389775d91dd392e4e0b97cd
Kolya0101 <85932161+Kolya0101@users.noreply.github.com>
2023-04-14 19:38:55 +0300

    laba1

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -17,7 +17,7 @@ function Signup({isAuthenticated, setIsAuthenticated}) {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://54.198.48.236:3001/api/auth/signup', {username, password});
+      const response = await axios.post('http://52.91.102.10:3001/api/auth/signup', {username, password});
       sessionStorage.setItem('token', response.data.token);
       sessionStorage.setItem('name', response.data.username);
       setIsAuthenticated(true);
--- frontend/src/components/todo/AddTodo.js
+++ frontend/src/components/todo/AddTodo.js
@@ -19,7 +19,7 @@ function AddTodo({isAuthenticated, setIsAuthenticated}) {
     e.preventDefault();
 
     try {
-      await axios.post('http://54.198.48.236:3001/api/todo', {title, targetDate}, {
+      await axios.post('http://52.91.102.10:3001/api/todo', {title, targetDate}, {
         headers: {
           'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
         }
