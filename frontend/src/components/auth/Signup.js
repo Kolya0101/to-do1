5dd10f37f89cd7285ad614accc8c90c355fc8018
Kolya0101 <85932161+Kolya0101@users.noreply.github.com>
2023-04-14 10:14:28 +0300

    laba1

--- frontend/src/components/auth/Signup.js
+++ frontend/src/components/auth/Signup.js
@@ -17,7 +17,7 @@ function Signup({isAuthenticated, setIsAuthenticated}) {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://54.172.145.106:3000/api/auth/signup', {username, password});
+      const response = await axios.post('http://52.3.247.194:3000/api/auth/signup', {username, password});
       sessionStorage.setItem('token', response.data.token);
       sessionStorage.setItem('name', response.data.username);
       setIsAuthenticated(true);
